57d725a31943b981ae0a8d1a9f2ec21bcf600ae4
Darby Frey <darbyfrey@gmail.com>
2015-09-08 16:11:29 -0500

    remove mongo specific queries

--- app/controllers/delayed_job_admin_controller.rb
+++ app/controllers/delayed_job_admin_controller.rb
@@ -3,8 +3,6 @@ class DelayedJobAdminController < Admin::AdminController
   layout 'delayed_job_admin'
 
   def index
-    @jobs = Delayed::Job.asc(:priority).asc(:run_at).limit(50)
-    @count = Delayed::Job.count
   end
 
 end
